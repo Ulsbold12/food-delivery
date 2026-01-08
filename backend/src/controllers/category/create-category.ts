142e18a20dfd9a1f971bfa5f271075b0e7120ec4
Ulsbold12 <ulsboldulsbold921@gmail.com>
2026-01-08 14:19:28 +0800

    cs

--- backend/src/controllers/category/create-category.ts
+++ backend/src/controllers/category/create-category.ts
@@ -5,9 +5,7 @@ import { CategoryModel } from "../../database/schema/category.schema.ts";
 export const createCategory: RequestHandler = async (req, res) => {
   const body = req.body;
 
-  const category = await CategoryModel.create({
-    name: body.name,
-  });
+  const category = await CategoryModel.insertMany(body);
 
   res.status(201).json(category);
 };
